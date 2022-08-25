ed8705309fa7a92f028b51954c670a541f1fbd68
ChauNguyen97 <50213352+ChauNguyen97@users.noreply.github.com>
2022-08-25 23:10:45 +0700

    add gui server

---
+++ Server/src/Server.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Server {
+    private JTextArea textHost;
+    private JPanel hostZone;
+    private JButton buttonConnect;
+    private JPanel searchLog;
+    private JTextField textSearch;
+    private JButton buttonSearch;
+    private JList listClient;
+    private JPanel clientZone;
+    private JButton buttonShow;
+    private JButton buttonChange;
+    private JPanel buttonClientzone;
+    private JTextField textField1;
+    private JTable table1;
+}
